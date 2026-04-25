2bc0899f18ccea01c76c8e8a0bdc58ba530b58d5
Frivcoom111 <carloseduardofaustino67@gmail.com>
2026-04-25 10:01:00 -0300

    Funcionalidade: inicializar aplicativo Express com middleware de autenticação e tratamento de erros global.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import categoriesRoutes from "./routes/categoriesRoutes.js";
 import productsRoutes from "./routes/productsRoutes.js";
+import { errorMiddlware } from "./middlewares/errorMiddlewares.js";
 
 const app = express();
 app.use(express.json());
@@ -11,5 +12,8 @@ app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/categories", categoriesRoutes);
 app.use("/products", productsRoutes);
+
+// Usado para captura de errors e retornar em JSON ao invés de HTML assim como o Express retorna.
+app.use(errorMiddlware); // ← sempre o ÚLTIMO
 
 export default app;
--- src/middlewares/authMiddlewares.js
+++ src/middlewares/authMiddlewares.js
@@ -4,40 +4,40 @@ import { getRequiredEnv } from "../utils/getRequeridEnv.js";
 
 const JWT_SECRET = getRequiredEnv("JWT_SECRET");
 
+const createError = (status, message) => {
+  const err = new Error(message);
+  err.status = status;
+  return err;
+};
+
 export const authToken = (req, res, next) => {
   try {
     const headerAuthorization = req.headers.authorization;
 
-    // Verificação do token JWT.
     if (!headerAuthorization || !headerAuthorization.startsWith("Bearer ")) {
-      return res.status(401).json({ message: "Token não fornecido" });
+      return next(createError(401, "Token não fornecido."));
     }
 
     const token = headerAuthorization.split(" ")[1];
 
     const decoded = jwt.verify(token, JWT_SECRET);
 
-    // Verifica se o usuário está desativado.
-    if (!decoded.isActive) return res.status(401).json({ message: "Usuário desativado, acesso negado." });
+    if (!decoded.isActive) return next(createError(401, "Usuário desativado, acesso negado."));
 
     req.user = decoded; // { id, role, isActive }
 
     next();
   } catch (error) {
-    res.status(401).json({ message: "Token inválido ou expirado" });
+    next(createError(401, "Token inválido ou expirado."));
   }
 };
 
 export const authAdminOnly = (req, res, next) => {
-  try {
-    const role = req.user.role;
+  const role = req.user?.role;
 
-    if (!role || role !== "ADMIN") {
-      return res.status(403).json({ message: "Acesso admin negado." });
-    }
-
-    next();
-  } catch (error) {
-    res.status(403).json({ message: "Acesso admin negado." });
+  if (!role || role !== "ADMIN") {
+    return next(createError(403, "Acesso admin negado."));
   }
+
+  next();
 };
