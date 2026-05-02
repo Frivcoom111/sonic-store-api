2384dca0bb0c64262a881a671cab39dc52ea50ac
Frivcoom111 <carloseduardofaustino67@gmail.com>
2026-05-02 00:43:51 -0300

    Funcionalidade: inicializar aplicação Express com dependências de segurança, limitação de taxa e documentação Swagger.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import { rateLimit } from "express-rate-limit";
 import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 import { swaggerOptions } from "./config/swagger.js";
+import { errorMiddlware } from "./middlewares/errorMiddlewares.js";
+
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import categoriesRoutes from "./routes/categoriesRoutes.js";
@@ -9,14 +14,43 @@ import productsRoutes from "./routes/productsRoutes.js";
 import cartRoutes from "./routes/cartRoutes.js";
 import addressRoutes from "./routes/addressRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
-import { errorMiddlware } from "./middlewares/errorMiddlewares.js";
 
 const app = express();
-app.use(express.json({ limit: "10kb" }));
 
+// Quem pode acessar sua API
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL ?? "*",
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+  })
+);
+
+// Segurança: cabeçalhos HTTP protegidos
+app.use(helmet());
+
+// Limite de requisições por IP
+const defaultLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  message: { error: "Muitas requisições. Tente novamente em 15 minutos." },
+});
+
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 10,
+  message: { error: "Muitas tentativas. Tente novamente em 15 minutos." },
+});
+
+app.use(defaultLimiter);
+app.use("/auth", authLimiter)
+
+app.use(express.json({ limit: "10kb" })); // Limite de arquivos que podem ser enviados na request.
+
+// Configuração do swagger para documentar minha API.
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
+// Rotas API.
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/categories", categoriesRoutes);
